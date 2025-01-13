cee3ae9f09e9ec1b3cff28165b0175351443dd3f
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-01-13 09:58:02 +0530

    same

--- src/common/BackToTop.jsx
+++ src/common/BackToTop.jsx
@@ -16,19 +16,14 @@ const BackToTop = () => {
         });
     };
     return (
-        <div>
+        <>
             {isVisible && (
                 <button onClick={scrollToTop}
                     className="fixed bottom-5 right-5 z-20 bg-white p-2 w-12 border-black border rounded-md flex justify-center">
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="black" class="bi bi-chevron-double-up" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708z" />
-                            <path fill-rule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708z" />
-                        </svg>
-                    </span>
+                        <img src="./assets/images/svg/backtotop.svg" alt="backtotop" />
                 </button>
             )}
-        </div>
+        </>
     );
 };
 export default BackToTop;
